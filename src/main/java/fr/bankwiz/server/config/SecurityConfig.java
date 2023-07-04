1adfbd2d4932d4f85889eb50d1f952a2b7bc7610
Jean-Baptiste WITTNER <jb.wittner@outlook.com>
2023-07-04 06:14:04 +0000

    should work I guess...

--- src/main/java/fr/bankwiz/server/config/SecurityConfig.java
+++ src/main/java/fr/bankwiz/server/config/SecurityConfig.java
@@ -68,7 +68,9 @@ public class SecurityConfig {
                 .requestMatchers("/status/admin").hasAuthority("SCOPE_admin:configuration")
                 .anyRequest().authenticated()
             )
-            .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter()).decoder(jwtDecoder())));
+            .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> 
+            jwt.jwtAuthenticationConverter(jwtAuthenticationConverter()).decoder(jwtDecoder()))
+            );
         return http.build();
     }
 
