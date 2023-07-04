8583e29c84045ad58e2605a60f9928d1c551c260
Jean-Baptiste WITTNER <jb.wittner@outlook.com>
2023-07-04 09:42:00 +0000

    typo

--- src/test/java/fr/bankwiz/server/ServerApplicationTests.java
+++ src/test/java/fr/bankwiz/server/ServerApplicationTests.java
@@ -3,22 +3,18 @@ package fr.bankwiz.server;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.client.AutoConfigureWebClient;
-import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.context.ApplicationContext;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.test.web.reactive.server.SecurityMockServerConfigurers;
 import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
 import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers;
-import org.springframework.test.web.reactive.server.WebTestClient;
-import org.springframework.test.web.reactive.server.WebTestClientConfigurer;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.test.web.servlet.setup.MockMvcConfigurer;
 import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ServerApplicationTests {
@@ -33,48 +29,25 @@ class ServerApplicationTests {
 
     @Test
     void contextLoads() throws Exception {
-        final String baseUrl = "http://localhost:" + port;
-        //this.webTestClient = WebTestClient.bindToServer().baseUrl(baseUrl).build();
 
-        mvc = MockMvcBuilders
-				.webAppContextSetup(context)
-                .apply(SecurityMockMvcConfigurers.springSecurity())
-				.build();
+        mvc.perform(MockMvcRequestBuilders.get("/status/toto")).andExpect(status().isUnauthorized());
+        mvc.perform(MockMvcRequestBuilders.get("/status/toto").with(SecurityMockMvcRequestPostProcessors.jwt()))
+                .andExpect(status().isNotFound());
 
-        mvc.perform(MockMvcRequestBuilders.get("/status/toto")).andExpect(matcher -> {
-            System.out.println(matcher.getResponse().getStatus());
-        });
+        mvc.perform(MockMvcRequestBuilders.get("/status/public")).andExpect(status().isOk());
 
-        mvc.perform(MockMvcRequestBuilders.get("/status/toto"))
+        mvc.perform(MockMvcRequestBuilders.get("/status/private")).andExpect(status().isUnauthorized());
+        mvc.perform(MockMvcRequestBuilders.get("/status/private").with(SecurityMockMvcRequestPostProcessors.jwt()))
+                .andExpect(status().isOk());
 
-        mvc.perform(MockMvcRequestBuilders.get("/status/public")).andExpect(matcher -> {
-            System.out.println(matcher.getResponse().getStatus());
-        });
-
-        mvc.perform(MockMvcRequestBuilders.get("/status/private")).andExpect(matcher -> {
-            System.out.println(matcher.getResponse().getStatus());
-        });
-
-        mvc.perform(MockMvcRequestBuilders.get("/status/private").with(SecurityMockMvcRequestPostProcessors.jwt())).andExpect(matcher -> {
-            System.out.println(matcher.getResponse().getStatus());
-        });
-
-        mvc.perform(MockMvcRequestBuilders.get("/status/admin").with(SecurityMockMvcRequestPostProcessors.jwt())).andExpect(matcher -> {
-            System.out.println(matcher.getResponse().getStatus());
-        });
-
-        mvc.perform(MockMvcRequestBuilders.get("/status/admin").with(SecurityMockMvcRequestPostProcessors.jwt().authorities(new SimpleGrantedAuthority("SCOPE_admin:configuration")).jwt(jwt -> {
-            System.out.println(jwt);
-        }))).andExpect(matcher -> {
-            System.out.println(matcher.getResponse().getStatus());
-        });
-
-        mvc.perform(MockMvcRequestBuilders.get("/status/admin").with(SecurityMockMvcRequestPostProcessors.jwt().authorities(new SimpleGrantedAuthority("SCOPE_admin:configurationdqsdqd")).jwt(jwt -> {
-            System.out.println(jwt);
-        }))).andExpect(matcher -> {
-            System.out.println(matcher.getResponse().getStatus());
-        });
-
-        Assertions.assertTrue(true);
+        mvc.perform(MockMvcRequestBuilders.get("/status/admin")).andExpect(status().isUnauthorized());
+        mvc.perform(MockMvcRequestBuilders.get("/status/admin").with(SecurityMockMvcRequestPostProcessors.jwt()))
+                .andExpect(status().isForbidden());
+        mvc.perform(MockMvcRequestBuilders.get("/status/admin").with(SecurityMockMvcRequestPostProcessors.jwt()))
+                .andExpect(status().isForbidden());
+        mvc.perform(MockMvcRequestBuilders.get("/status/admin")
+                        .with(SecurityMockMvcRequestPostProcessors.jwt()
+                                .authorities(new SimpleGrantedAuthority("SCOPE_admin:configuration"))))
+                .andExpect(status().isOk());
     }
 }
