5725a71aae71f830de7068ea15c65ff809669d76
Jean-Baptiste WITTNER <jb.wittner@outlook.com>
2023-07-11 17:56:13 +0200

    more ignored words

--- src/main/java/fr/bankwiz/server/model/User.java
+++ src/main/java/fr/bankwiz/server/model/User.java
@@ -1,2 +1,19 @@
-package fr.bankwiz.server.model;public class User {
+package fr.bankwiz.server.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    protected Integer userId;
+    private String firstName;
+    private String lastName;
+    private String email;
+
 }
