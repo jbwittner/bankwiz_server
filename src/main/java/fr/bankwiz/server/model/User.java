4987379795c7c2646748dd83136a297b67c7ecde
Jean-Baptiste WITTNER <jb.wittner@outlook.com>
2023-07-11 23:18:29 +0200

    Final commit, ready for tagging

--- src/main/java/fr/bankwiz/server/model/User.java
+++ src/main/java/fr/bankwiz/server/model/User.java
@@ -1,5 +1,6 @@
 package fr.bankwiz.server.model;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -9,11 +10,22 @@ import lombok.NoArgsConstructor;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table(name = "USER_ACCOUNT")
 public class User {
 
+    @Id
+    @Column(name = "USER_ID", nullable = false, updatable = false, insertable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     protected Integer userId;
+
+    @Column(name = "FIRST_NAME", nullable = false, length = 60)
     private String firstName;
+
+    @Column(name = "LAST_NAME", nullable = false, length = 60)
     private String lastName;
+
+    @Column(name = "EMAIL", nullable = false)
     private String email;
 
 }
