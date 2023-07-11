7a68e460b0476930d2ec0fa49d3841e819831853
Jean-Baptiste WITTNER <jb.wittner@outlook.com>
2023-07-11 18:33:05 +0200

    debugo

--- src/test/java/fr/bankwiz/server/integrationtest/testhelper/IntegrationTestBase.java
+++ src/test/java/fr/bankwiz/server/integrationtest/testhelper/IntegrationTestBase.java
@@ -3,9 +3,23 @@ package fr.bankwiz.server.integrationtest.testhelper;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.containers.MySQLContainer;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.utility.DockerImageName;
 
+@ActiveProfiles("test")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public abstract class IntegrationTestBase {
+
+    @ServiceConnection
+    private static final MySQLContainer MY_SQL_CONTAINER;
+
+    static {
+        MY_SQL_CONTAINER = new MySQLContainer(DockerImageName.parse("mysql:8.0.33"));
+        //MY_SQL_CONTAINER.withInitScript("databases.sql");
+        MY_SQL_CONTAINER.start();
+    }
 
     @Autowired
     protected IntegrationMVCClient client;
