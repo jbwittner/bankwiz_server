bd7a2b14b5c3e7c3ac4e649cfd616e74970c00f0
Jean-Baptiste WITTNER <44366963+jbwittner@users.noreply.github.com>
2023-07-12 15:40:36 +0200

    add db (#10)
    * Add jpa
    
    * more ignored words
    
    * debugo
    
    * Final commit, ready for tagging
    
    * Shovelling coal into the server...
    
    * I did it for the lulz!
    
    * I must enjoy torturing myself
    
    * yolo push
    
    * ID:10T Error

--- src/test/java/fr/bankwiz/server/integrationtest/testhelper/IntegrationTestBase.java
+++ src/test/java/fr/bankwiz/server/integrationtest/testhelper/IntegrationTestBase.java
@@ -3,9 +3,24 @@ package fr.bankwiz.server.integrationtest.testhelper;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.utility.DockerImageName;
 
+@ActiveProfiles("test")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public abstract class IntegrationTestBase {
+
+    @ServiceConnection
+    private static final MySQLContainer<?> MY_SQL_CONTAINER;
+
+    private static final String MYSQL_IMAGE_NAME = "mysql:8.0.33";
+
+    static {
+        MY_SQL_CONTAINER = new MySQLContainer<>(DockerImageName.parse(MYSQL_IMAGE_NAME));
+        MY_SQL_CONTAINER.start();
+    }
 
     @Autowired
     protected IntegrationMVCClient client;
