4610e06353f4a6b2c24c003ef57de2d7b68675c7
Jean-Baptiste WITTNER <jb.wittner@outlook.com>
2023-07-17 17:33:08 +0200

    FUCKING CUBE5

--- src/test/java/fr/bankwiz/server/integrationtest/controller/groupcontroller/AddUserToGroupTest.java
+++ src/test/java/fr/bankwiz/server/integrationtest/controller/groupcontroller/AddUserToGroupTest.java
@@ -1,9 +1,15 @@
 package fr.bankwiz.server.integrationtest.controller.groupcontroller;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import fr.bankwiz.openapi.model.AddUserGroupRequest;
 import fr.bankwiz.openapi.model.GroupAuthorizationEnum;
+import fr.bankwiz.openapi.model.GroupDTO;
+import fr.bankwiz.openapi.model.UserDTO;
+import fr.bankwiz.server.integrationtest.testhelper.IntegrationMVCClient;
 import fr.bankwiz.server.integrationtest.testhelper.IntegrationTestBase;
 import fr.bankwiz.server.model.Group;
 import fr.bankwiz.server.model.User;
@@ -13,20 +19,31 @@ public class AddUserToGroupTest extends IntegrationTestBase {
 
     @Override
     protected void initDataBeforeEach() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'initDataBeforeEach'");
     }
 
     @Test
-    void addUserOk() {
+    void addUserOk() throws Exception {
         final User admin = this.integrationTestFactory.getUser();
         final Group group = this.integrationTestFactory.getGroupWithRigh(admin, GroupRightEnum.ADMIN);
+        final Integer groupId = group.getGroupId();
 
         final User userToAdd = this.integrationTestFactory.getUser();
         final Integer userToAddId = userToAdd.getUserId();
 
         final AddUserGroupRequest addUserGroupRequest =
                 new AddUserGroupRequest(userToAddId, GroupAuthorizationEnum.READ);
+
+        var result = this.client.doPost(IntegrationMVCClient.UriEnum.GROUP_ID_USER.getUri(groupId), admin.getAuthId(), addUserGroupRequest)
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andReturn();
+
+        final GroupDTO groupDTO = IntegrationMVCClient.convertMvcResultToResponseObject(result, GroupDTO.class);
+
+                Assertions.assertAll(
+                () -> Assertions.assertEquals(2, groupDTO.getUsers().size()));
+
+
 
     }
     
