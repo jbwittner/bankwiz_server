1c26491ffca7b82c698ca44d1e44bb0e872244db
Jean-Baptiste WITTNER <jb.wittner@outlook.com>
2023-07-26 18:10:38 +0200

    Merge pull my finger request

--- src/main/java/fr/bankwiz/server/repository/GroupRightRepository.java
+++ src/main/java/fr/bankwiz/server/repository/GroupRightRepository.java
@@ -1,11 +1,11 @@
 package fr.bankwiz.server.repository;
 
-import fr.bankwiz.server.model.Group;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import fr.bankwiz.server.model.Group;
 import fr.bankwiz.server.model.GroupRight;
-
-import java.util.List;
 
 public interface GroupRightRepository extends JpaRepository<GroupRight, Integer> {
     List<GroupRight> findAllByGroup(Group group);
