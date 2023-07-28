a4cdab09930c3542d2a0d21cabbe0e1442bd8295
Jean-Baptiste WITTNER <jb.wittner@outlook.com>
2023-07-28 23:53:21 +0200

    To be honest, I do not quite remember everything I changed here today. But it is all good, I tell ya.

--- src/test/java/fr/bankwiz/server/unittest/service/groupservice/DeleteGroupTest.java
+++ src/test/java/fr/bankwiz/server/unittest/service/groupservice/DeleteGroupTest.java
@@ -2,11 +2,14 @@ package fr.bankwiz.server.unittest.service.groupservice;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
+import fr.bankwiz.server.exception.GroupNotExistException;
+import fr.bankwiz.server.exception.UserNotAdminException;
 import fr.bankwiz.server.model.Group;
 import fr.bankwiz.server.model.GroupRight;
 import fr.bankwiz.server.model.User;
@@ -60,5 +63,34 @@ public class DeleteGroupTest extends UnitTestBase {
                 () -> Assertions.assertEquals(0, group.getGroupRights().size()),
                 () -> Assertions.assertEquals(0, user.getGroupRights().size()),
                 () -> Assertions.assertEquals(0, anotherUser.getGroupRights().size()));
+    }
+
+    @Test
+    void userNotAdminException() {
+        final User user = this.unitTestFactory.getUser();
+        final Group group = this.unitTestFactory.getGroupWithRight(user, GroupRight.GroupRightEnum.WRITE);
+
+        final Integer groupId = group.getGroupId();
+
+        Mockito.when(this.mockAuthenticationFacade.getCurrentUser()).thenReturn(user);
+        this.groupRepositoryMockFactory.mockFindById(groupId, group);
+
+        Assertions.assertThrows(UserNotAdminException.class, () -> {
+            this.groupService.deleteGroup(groupId);
+        });
+    }
+
+    @Test
+    void groupNotExistException() {
+        final User user = this.unitTestFactory.getUser();
+
+        final Integer groupId = this.faker.random().nextInt(Integer.MAX_VALUE);
+
+        Mockito.when(this.mockAuthenticationFacade.getCurrentUser()).thenReturn(user);
+        this.groupRepositoryMockFactory.mockFindById(groupId, Optional.empty());
+
+        Assertions.assertThrows(GroupNotExistException.class, () -> {
+            this.groupService.deleteGroup(groupId);
+        });
     }
 }
