9b40d4c1735a42406d7b9a65886fdfb5ac8d9b6b
Jean-Baptiste WITTNER <jb.wittner@outlook.com>
2023-10-04 19:52:01 +0200

    added super-widget 2.0.

--- infrastructure/src/main/java/fr/bankwiz/server/infrastructure/Infrastructure.java
+++ infrastructure/src/main/java/fr/bankwiz/server/infrastructure/Infrastructure.java
@@ -3,6 +3,6 @@ package fr.bankwiz.server.infrastructure;
 public class Infrastructure {
 
     public static void main(String[] args) {
-        System.out.println("Domain");
+        System.out.println("Infrastructure");
     }
 }
