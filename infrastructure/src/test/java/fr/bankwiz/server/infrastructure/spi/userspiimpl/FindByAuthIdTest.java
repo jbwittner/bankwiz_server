19a89ff9e8b8d533cd02c3c978a1d5bd2fc706cf
Jean-Baptiste WITTNER <jbwittner.ext@hipay.com>
2023-10-17 11:21:33 +0000

    Never before had a small typo like this one caused so much damage.

--- infrastructure/src/test/java/fr/bankwiz/server/infrastructure/spi/userspiimpl/FindByAuthIdTest.java
+++ infrastructure/src/test/java/fr/bankwiz/server/infrastructure/spi/userspiimpl/FindByAuthIdTest.java
@@ -1,5 +1,13 @@
 package fr.bankwiz.server.infrastructure.spi.userspiimpl;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class FindByAuthIdTest {
+
+    @Test
+    void toto() {
+        Assertions.assertTrue(true);
+    }
     
 }
