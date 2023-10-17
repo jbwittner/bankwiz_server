bade51b4d7efb7b024efbc23f66310a2bea5cde2
Jean-Baptiste WITTNER <jb.wittner@outlook.com>
2023-10-17 23:58:53 +0200

    last hope failed, maybe now

--- infrastructure/src/test/java/fr/bankwiz/server/infrastructure/transformer/usertransformer/ToUserDTOTest.java
+++ infrastructure/src/test/java/fr/bankwiz/server/infrastructure/transformer/usertransformer/ToUserDTOTest.java
@@ -1,5 +1,24 @@
 package fr.bankwiz.server.infrastructure.transformer.usertransformer;
 
-public class ToUserDTOTest {
-    
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import fr.bankwiz.openapi.model.UserDTO;
+import fr.bankwiz.server.domain.model.User;
+import fr.bankwiz.server.infrastructure.testhelper.InfrastructureUnitTestBase;
+import fr.bankwiz.server.infrastructure.transformer.UserTransformer;
+
+class ToUserDTOTest extends InfrastructureUnitTestBase {
+
+    @Override
+    protected void initDataBeforeEach() {}
+
+    @Test
+    void ok() {
+        final User user = this.factory.getUser();
+        final UserDTO userDTO = UserTransformer.toUserDTO(user);
+        Assertions.assertAll(
+                () -> Assertions.assertEquals(user.getEmail(), userDTO.getEmail()),
+                () -> Assertions.assertEquals(user.getUserUuid(), userDTO.getId()));
+    }
 }
