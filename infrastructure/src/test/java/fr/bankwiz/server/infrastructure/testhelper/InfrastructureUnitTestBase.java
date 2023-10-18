835905fccd5912b41f19bd015a6e0b9437d249b6
Jean-Baptiste WITTNER <jb.wittner@outlook.com>
2023-10-18 16:41:08 +0200

    fix some fucking errors

--- infrastructure/src/test/java/fr/bankwiz/server/infrastructure/testhelper/InfrastructureUnitTestBase.java
+++ infrastructure/src/test/java/fr/bankwiz/server/infrastructure/testhelper/InfrastructureUnitTestBase.java
@@ -2,19 +2,15 @@ package fr.bankwiz.server.infrastructure.testhelper;
 
 import org.junit.jupiter.api.BeforeEach;
 
-import fr.bankwiz.server.infrastructure.testhelper.mock.MockUserSpi;
-
 public abstract class InfrastructureUnitTestBase {
 
     protected InfrastructureFaker faker;
     protected InfrastructureUnitTestFactory factory;
-    protected MockUserSpi mockUserSpi;
 
     @BeforeEach
     public void beforeEach() {
         this.faker = new InfrastructureFaker();
         this.factory = new InfrastructureUnitTestFactory(faker);
-        this.mockUserSpi = new MockUserSpi();
         this.initDataBeforeEach();
     }
 
