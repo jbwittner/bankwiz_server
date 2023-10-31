03a7864749f6beff3fd294bdf2bbf695b20b5b64
Jean-Baptiste WITTNER <jb.wittner@outlook.com>
2023-10-31 22:42:48 +0100

    Never before had a small typo like this one caused so much damage.

---
+++ domain/src/test/java/fr/bankwiz/server/domain/service/groupdomainservice/GroupCreationTest.java
@@ -0,0 +1,11 @@
+package fr.bankwiz.server.domain.service.groupdomainservice;
+
+import fr.bankwiz.server.domain.testhelper.DomainUnitTestBase;
+
+public class GroupCreationTest extends DomainUnitTestBase {
+
+    @Override
+    protected void initDataBeforeEach() {
+    }
+    
+}
