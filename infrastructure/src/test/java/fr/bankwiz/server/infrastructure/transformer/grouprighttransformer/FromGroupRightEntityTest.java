40a92d9e40a3a391b8b5a69f21940918473df398
Jean-Baptiste WITTNER <jb.wittner@outlook.com>
2023-11-01 23:20:49 +0100

    If it's stupid and it works, it ain't stupid

--- infrastructure/src/test/java/fr/bankwiz/server/infrastructure/transformer/grouprighttransformer/FromGroupRightEntityTest.java
+++ infrastructure/src/test/java/fr/bankwiz/server/infrastructure/transformer/grouprighttransformer/FromGroupRightEntityTest.java
@@ -3,14 +3,15 @@ package fr.bankwiz.server.infrastructure.transformer.grouprighttransformer;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import fr.bankwiz.openapi.model.GroupAuthorizationEnum;
+import fr.bankwiz.server.domain.model.data.Group;
 import fr.bankwiz.server.domain.model.data.GroupRight;
+import fr.bankwiz.server.domain.model.data.User;
 import fr.bankwiz.server.infrastructure.spi.database.entity.GroupEntity;
 import fr.bankwiz.server.infrastructure.spi.database.entity.GroupRightEntity;
 import fr.bankwiz.server.infrastructure.spi.database.entity.GroupRightEntity.GroupRightEntityEnum;
+import fr.bankwiz.server.infrastructure.spi.database.entity.UserEntity;
 import fr.bankwiz.server.infrastructure.testhelper.InfrastructureUnitTestBase;
 import fr.bankwiz.server.infrastructure.transformer.GroupRightTransformer;
-import fr.bankwiz.server.infrastructure.transformer.GroupTransformer;
 
 class FromGroupRightEntityTest extends InfrastructureUnitTestBase {
 
@@ -22,7 +23,25 @@ class FromGroupRightEntityTest extends InfrastructureUnitTestBase {
         final GroupRightEntity groupRightEntity = this.factory.getGroupRightEntity(GroupRightEntityEnum.WRITE);
         final GroupRight groupRight = GroupRightTransformer.fromGroupRightEntity(groupRightEntity);
         Assertions.assertAll(
-                () -> Assertions.assertEquals(group.getGroupName(), groupEntity.getGroupName()),
-                () -> Assertions.assertEquals(group.getGroupUuid(), groupEntity.getGroupId()));
+                () -> Assertions.assertEquals(
+                        groupRightEntity.getGroupRightEntityEnum().name(),
+                        groupRight.getGroupRightEnum().name()),
+                () -> {
+                    final UserEntity userEntity = groupRightEntity.getUserEntity();
+                    final User user = groupRight.getUser();
+
+                    Assertions.assertAll(
+                            () -> Assertions.assertEquals(userEntity.getEmail(), user.getEmail()),
+                            () -> Assertions.assertEquals(userEntity.getAuthId(), user.getAuthId()),
+                            () -> Assertions.assertEquals(userEntity.getUserId(), user.getUserUuid()));
+                },
+                () -> {
+                    final GroupEntity groupEntity = groupRightEntity.getGroupEntity();
+                    final Group group = groupRight.getGroup();
+
+                    Assertions.assertAll(
+                            () -> Assertions.assertEquals(groupEntity.getGroupName(), group.getGroupName()),
+                            () -> Assertions.assertEquals(groupEntity.getGroupId(), group.getGroupUuid()));
+                });
     }
 }
