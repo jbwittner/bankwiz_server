bce6cba6b8222d1cbd189b3aa18bdaaf15cdb0bd
Jean-Baptiste WITTNER <jb.wittner@outlook.com>
2023-11-01 23:12:33 +0100

    wrapping should work properly. Probably.

---
+++ infrastructure/src/test/java/fr/bankwiz/server/infrastructure/transformer/grouptransformer/ToGroupEntityTest.java
@@ -0,0 +1,24 @@
+package fr.bankwiz.server.infrastructure.transformer.grouptransformer;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import fr.bankwiz.server.domain.model.data.Group;
+import fr.bankwiz.server.infrastructure.spi.database.entity.GroupEntity;
+import fr.bankwiz.server.infrastructure.testhelper.InfrastructureUnitTestBase;
+import fr.bankwiz.server.infrastructure.transformer.GroupTransformer;
+
+class ToGroupEntityTest extends InfrastructureUnitTestBase {
+
+    @Override
+    protected void initDataBeforeEach() {}
+
+    @Test
+    void ok() {
+        final Group group = this.factory.getGroup();
+        final GroupEntity groupEntity = GroupTransformer.toGroupEntity(group);
+        Assertions.assertAll(
+                () -> Assertions.assertEquals(group.getGroupName(), groupEntity.getGroupName()),
+                () -> Assertions.assertEquals(group.getGroupUuid(), groupEntity.getGroupId()));
+    }
+}
