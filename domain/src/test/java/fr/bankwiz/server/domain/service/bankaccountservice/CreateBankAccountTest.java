976952756c9d588052a7f3f5f9cf1e80e300adf6
Jean-Baptiste WITTNER <jb.wittner@outlook.com>
2023-11-24 20:50:33 +0100

    haha yes it works now

---
+++ domain/src/test/java/fr/bankwiz/server/domain/service/bankaccountservice/CreateBankAccountTest.java
@@ -0,0 +1,64 @@
+package fr.bankwiz.server.domain.service.bankaccountservice;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import fr.bankwiz.server.domain.model.data.BankAccount;
+import fr.bankwiz.server.domain.model.data.Group;
+import fr.bankwiz.server.domain.model.data.GroupRight;
+import fr.bankwiz.server.domain.model.data.GroupRight.GroupRightEnum;
+import fr.bankwiz.server.domain.model.data.User;
+import fr.bankwiz.server.domain.model.input.BankAccountCreationInput;
+import fr.bankwiz.server.domain.service.BankAccountService;
+import fr.bankwiz.server.domain.testhelper.DomainUnitTestBase;
+import fr.bankwiz.server.domain.tools.CheckRightTools;
+
+public class CreateBankAccountTest extends DomainUnitTestBase {
+
+    private BankAccountService bankAccountService;
+
+    @Override
+    protected void initDataBeforeEach() {
+        final CheckRightTools checkRightTools = new CheckRightTools(this.mockGroupRightSpi.getMock());
+        this.bankAccountService = new BankAccountService(
+                this.mockBankAccountSpi.getMock(),
+                this.mockGroupSpi.getMock(),
+                this.mockAuthenticationSpi.getMock(),
+                checkRightTools);
+    }
+
+    @Test
+    void createBankAccountOk() {
+        final User user = this.factory.getUser();
+        final Group group = this.factory.getGroup();
+        final List<GroupRight> groupRights = new ArrayList<>();
+        groupRights.add(this.factory.getGroupRight(user, GroupRightEnum.WRITE));
+
+        this.mockGroupSpi.mockFindById(group.getGroupId(), Optional.of(group));
+        this.mockAuthenticationSpi.mockGetCurrentUser(user);
+        this.mockGroupRightSpi.mockFindByGroup(group, groupRights);
+        this.mockBankAccountSpi.mockSave();
+
+        final String bankAccountName = this.faker.superhero().name();
+        final UUID groupId = group.getGroupId();
+        final Integer decimalBaseAmount = this.faker.random().nextInt(Integer.MAX_VALUE);
+
+        final BankAccountCreationInput bankAccountCreationInput = BankAccountCreationInput.builder()
+                .bankAccountName(bankAccountName)
+                .groupId(groupId)
+                .decimalBaseAmount(decimalBaseAmount)
+                .build();
+
+        final BankAccount bankAccount = this.bankAccountService.createBankAccount(bankAccountCreationInput);
+
+        Assertions.assertAll(
+                () -> Assertions.assertEquals(bankAccountName, bankAccount.getBankAccountName()),
+                () -> Assertions.assertEquals(group, bankAccount.getGroup()),
+                () -> Assertions.assertEquals(decimalBaseAmount, bankAccount.getDecimalBaseAmount()));
+    }
+}
