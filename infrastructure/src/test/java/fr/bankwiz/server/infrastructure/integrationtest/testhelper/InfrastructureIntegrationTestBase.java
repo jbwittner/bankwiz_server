41dbfd1080b549fec0c6db14e38d7b479fc0283d
Jean-Baptiste WITTNER <jb.wittner@outlook.com>
2023-12-01 12:09:02 +0100

    last hope failed, maybe now

--- infrastructure/src/test/java/fr/bankwiz/server/infrastructure/integrationtest/testhelper/InfrastructureIntegrationTestBase.java
+++ infrastructure/src/test/java/fr/bankwiz/server/infrastructure/integrationtest/testhelper/InfrastructureIntegrationTestBase.java
@@ -1,14 +1,29 @@
 package fr.bankwiz.server.infrastructure.integrationtest.testhelper;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.containers.MySQLContainer;
 
 import fr.bankwiz.server.infrastructure.spi.database.repository.UserEntityRepository;
 
 @SpringBootTest
 @ActiveProfiles(value = "test")
 public abstract class InfrastructureIntegrationTestBase {
+
+    static MySQLContainer<?> mySQLContainer = new MySQLContainer<>("mysql:8.0.33");
+
+    @BeforeAll
+    static void beforeAll() {
+        mySQLContainer.start();
+    }
+
+    @AfterAll
+    static void afterAll() {
+        mySQLContainer.stop();
+    }
 
     @Autowired
     protected UserEntityRepository userEntityRepository;
