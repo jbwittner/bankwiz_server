d5f9981ff4f592bfb4156d021ccb70ddd6471d59
Jean-Baptiste WITTNER <jb.wittner@outlook.com>
2023-12-02 00:14:05 +0100

    Testing in progress ;)

--- infrastructure/src/test/java/fr/bankwiz/server/infrastructure/integrationtest/api/usercontroller/CheckRegistrationTest.java
+++ infrastructure/src/test/java/fr/bankwiz/server/infrastructure/integrationtest/api/usercontroller/CheckRegistrationTest.java
@@ -2,15 +2,48 @@ package fr.bankwiz.server.infrastructure.integrationtest.api.usercontroller;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.security.test.web.reactive.server.SecurityMockServerConfigurers;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import io.restassured.http.ContentType;
+
+import java.util.Base64;
 
 import fr.bankwiz.server.infrastructure.integrationtest.testhelper.InfrastructureIntegrationTestBase;
 import io.restassured.RestAssured;
 
+import static io.restassured.RestAssured.given;
+
 class CheckRegistrationTest extends InfrastructureIntegrationTestBase {
+
+    public static String clientId = "some_client_id";
+    public static String redirectUri = "some_redirect_uri";
+    public static String scope = "some_scope";
+    public static String username = "some_email";
+    public static String password = "some_password";
+
+    public static String encode(String str1, String str2) {
+        return new String(Base64.getEncoder().encode((str1 + ":" + str2).getBytes()));
+    }
     
     @Test
     void ok() {
+        String authorization = encode(username, password);
+        var test = given().header("authorization", "Basic " + authorization)
+        .contentType(ContentType.URLENC)
+        .formParam("response_type", "code")
+        .queryParam("client_id", clientId)
+        .queryParam("redirect_uri", redirectUri)
+        .queryParam("scope", scope)
+        .post("/oauth2/authorize")
+        .then()
+        .statusCode(200)
+        .extract()
+        .response();
+
+        System.out.println(test);
+        //this.webTestClient.mutateWith(SecurityMockServerConfigurers.mockJwt()).get().uri("/user/checkregistration").exchange().expectStatus().is2xxSuccessful();
+        //this.webTestClient.get().uri("/user/checkregistration").exchange().expectStatus().is2xxSuccessful();
         var users = this.userEntityRepository.findAll();
         users.stream().forEach(user -> System.out.println("user: " + user));
         Assertions.assertTrue(true);
--- infrastructure/src/test/java/fr/bankwiz/server/infrastructure/integrationtest/testhelper/InfrastructureIntegrationTestBase.java
+++ infrastructure/src/test/java/fr/bankwiz/server/infrastructure/integrationtest/testhelper/InfrastructureIntegrationTestBase.java
@@ -6,31 +6,51 @@ import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.context.ApplicationContext;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.web.csrf.CsrfTokenRepository;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.client.MockMvcWebTestClient;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 import org.testcontainers.containers.MySQLContainer;
 
 import fr.bankwiz.server.infrastructure.spi.database.repository.UserEntityRepository;
 import io.restassured.RestAssured;
+
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles(value = "test")
 @AutoConfigureWebTestClient
 public abstract class InfrastructureIntegrationTestBase {
 
+    @Autowired
+	WebApplicationContext wac;
+
     static MySQLContainer<?> mySQLContainer;
 
-    protected WebTestClient webTestClient;
+    protected MockMvc mvc;
 
     @LocalServerPort
     private Integer port;
 
     @Autowired
     protected UserEntityRepository userEntityRepository;
+
+    @MockBean
+	// mock the JwtDecoder so that the jwks is not resolved since no AuthZ Server Setup
+	JwtDecoder jwtDecoder;
+
+	// Override the CsrfTokenRepository. Must explicitly wire CsrfTokenRepository Bean into DSL for this to work
+	@MockBean
+	CsrfTokenRepository csrfTokenRepository;
 
     static {
         mySQLContainer = new MySQLContainer<>("mysql:8.0.33");
@@ -55,9 +75,14 @@ public abstract class InfrastructureIntegrationTestBase {
     }
 
     @BeforeEach
-    void setUp(ApplicationContext context) {
+    void setUp() {
         RestAssured.baseURI = "http://localhost:" + port;
-        webTestClient = WebTestClient.bindToApplicationContext(context).build();
+        //webTestClient = MockMvcWebTestClient.bindToApplicationContext(this.wac).apply(springSecurity()).build();
+        //webTestClient = WebTestClient.bindToServer().baseUrl("http://localhost:" + port).build();
+        mvc = MockMvcBuilders
+				.webAppContextSetup(this.wac)
+				.apply(springSecurity()) 
+				.build();
     }
     
     
