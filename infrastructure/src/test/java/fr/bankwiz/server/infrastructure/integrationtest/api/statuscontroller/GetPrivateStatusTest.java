cee0e68ae6a6bcf5a0511648a62f72ae802d243e
Jean-Baptiste WITTNER <jbwittner.ext@hipay.com>
2023-12-04 13:14:40 +0000

    Nitpicking about alphabetizing methods, minor OCD thing

---
+++ infrastructure/src/test/java/fr/bankwiz/server/infrastructure/integrationtest/api/statuscontroller/GetPrivateStatusTest.java
@@ -0,0 +1,57 @@
+package fr.bankwiz.server.infrastructure.integrationtest.api.statuscontroller;
+
+import static io.restassured.RestAssured.given;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.security.oauth2.jwt.Jwt;
+
+import fr.bankwiz.server.infrastructure.integrationtest.testhelper.InfrastructureIntegrationTestBase;
+
+class GetPrivateStatusTest extends InfrastructureIntegrationTestBase {
+
+    @Test
+    void withoutAuthentification() {
+        given()
+                .get("/status/private")
+                .then()
+                .statusCode(401);
+    }
+
+    @Test
+    void withAuthentification() {
+        final Jwt jwt = Jwt.withTokenValue("token")
+                .header("alg", "none")
+                .claim("scope", "message:read")
+                .build();
+
+        Mockito.when(this.jwtDecoder.decode(jwt.getTokenValue())).thenReturn(jwt);
+
+        given().auth().oauth2(jwt.getTokenValue())
+                .get("/status/private")
+                .then()
+                .statusCode(200);
+    }
+
+    @Test
+    void withAdminScope() {
+        List<String> permissions = new ArrayList<>();
+        permissions.add("admin:configuration");
+
+        final Jwt jwt = Jwt.withTokenValue("token")
+                .header("alg", "none")
+                .claim("permissions", permissions)
+                .build();
+
+        Mockito.when(this.jwtDecoder.decode(jwt.getTokenValue())).thenReturn(jwt);
+
+        given().auth().oauth2(jwt.getTokenValue())
+                .get("/status/private")
+                .then()
+                .statusCode(200);
+    }
+
+}
