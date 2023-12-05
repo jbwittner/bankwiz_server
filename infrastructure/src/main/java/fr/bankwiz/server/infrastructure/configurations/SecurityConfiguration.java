a5e652ec4ac58b02d81d7652b4ab3cf75e50ddd3
Jean-Baptiste WITTNER <jb.wittner@outlook.com>
2023-12-05 23:50:26 +0100

    features

--- infrastructure/src/main/java/fr/bankwiz/server/infrastructure/configurations/SecurityConfiguration.java
+++ infrastructure/src/main/java/fr/bankwiz/server/infrastructure/configurations/SecurityConfiguration.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -51,6 +52,7 @@ public class SecurityConfiguration {
     }
 
     @Bean
+    @Primary
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(allowedOrigins);
