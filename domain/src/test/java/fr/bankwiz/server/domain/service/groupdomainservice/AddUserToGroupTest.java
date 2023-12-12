34da94bb0fc0ecac1d55fb61d2785873e8d5dd6c
Jean-Baptiste WITTNER <jb.wittner@outlook.com>
2023-12-12 12:02:02 +0100

    Spinning up the hamster...

--- domain/src/test/java/fr/bankwiz/server/domain/service/groupdomainservice/AddUserToGroupTest.java
+++ domain/src/test/java/fr/bankwiz/server/domain/service/groupdomainservice/AddUserToGroupTest.java
@@ -8,8 +8,10 @@ import java.util.UUID;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import fr.bankwiz.server.domain.exception.GroupNotExistException;
 import fr.bankwiz.server.domain.exception.UserAlreadyAccessGroupException;
 import fr.bankwiz.server.domain.exception.UserNotAdminException;
+import fr.bankwiz.server.domain.exception.UserNotExistException;
 import fr.bankwiz.server.domain.model.data.Group;
 import fr.bankwiz.server.domain.model.data.GroupRight;
 import fr.bankwiz.server.domain.model.data.GroupRight.GroupRightEnum;
@@ -104,6 +106,48 @@ class AddUserToGroupTest extends DomainUnitTestBase {
 
         Assertions.assertThrows(UserNotAdminException.class, () -> {
             this.groupDomainService.addUserToGroup(groupId, null);
+        });
+    }
+
+    @Test
+    void userNotExist() {
+        final User admin = this.factory.getUser();
+        final Group group = this.factory.getGroup();
+        final List<GroupRight> groupRights = new ArrayList<>();
+        groupRights.add(this.factory.getGroupRight(group, admin, GroupRightEnum.ADMIN));
+
+        this.mockAuthenticationSpi.mockGetCurrentUser(admin);
+        this.mockGroupSpi.mockFindById(group.getId(), Optional.of(group));
+        this.mockGroupRightSpi.mockFindByGroup(group, groupRights).mockSave();
+
+        final AddUserGroupInput addUserGroupInput = AddUserGroupInput.builder()
+                .userId(UUID.randomUUID())
+                .right(GroupRightEnum.WRITE)
+                .build();
+
+        final UUID groupId = group.getId();
+
+        Assertions.assertThrows(UserNotExistException.class, () -> {
+            this.groupDomainService.addUserToGroup(groupId, addUserGroupInput);
+        });
+    }
+
+    @Test
+    void groupNotExist() {
+        final User admin = this.factory.getUser();
+
+        final UUID groupId = UUID.randomUUID();
+
+        this.mockAuthenticationSpi.mockGetCurrentUser(admin);
+        this.mockGroupSpi.mockFindById(groupId, Optional.empty());
+
+        final AddUserGroupInput addUserGroupInput = AddUserGroupInput.builder()
+                .userId(UUID.randomUUID())
+                .right(GroupRightEnum.WRITE)
+                .build();
+
+        Assertions.assertThrows(GroupNotExistException.class, () -> {
+            this.groupDomainService.addUserToGroup(groupId, addUserGroupInput);
         });
     }
 }
--- infrastructure/src/test/java/fr/bankwiz/server/infrastructure/unittest/transformer/bankaccounttransformer/ToBankAccountIndexDTOTest.java
+++ infrastructure/src/test/java/fr/bankwiz/server/infrastructure/unittest/transformer/bankaccounttransformer/ToBankAccountIndexDTOTest.java
@@ -1,5 +1,9 @@
 package fr.bankwiz.server.infrastructure.unittest.transformer.bankaccounttransformer;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -15,7 +19,7 @@ class ToBankAccountIndexDTOTest extends InfrastructureUnitTestBase {
     protected void initDataBeforeEach() {}
 
     @Test
-    void ok() {
+    void toBankAccountIndexDTOSingle() {
         final Group group = this.factory.getGroup();
         final BankAccount bankAccount = this.factory.getBankAccount(group);
         final BankAccountIndexDTO bankAccountIndexDTO = BankAccountTransformer.toBankAccountIndexDTO(bankAccount);
@@ -24,5 +28,32 @@ class ToBankAccountIndexDTOTest extends InfrastructureUnitTestBase {
                 () -> Assertions.assertEquals(
                         bankAccount.getBankAccountName(), bankAccountIndexDTO.getBankAccountName()),
                 () -> Assertions.assertEquals(bankAccount.getId(), bankAccountIndexDTO.getBankAccountId()));
+    }
+
+    @Test
+    void toBankAccountIndexDTOList() {
+        final Group group = this.factory.getGroup();
+        final List<BankAccount> bankAccounts = new ArrayList<>();
+        bankAccounts.add(this.factory.getBankAccount(group));
+        bankAccounts.add(this.factory.getBankAccount(group));
+        final List<BankAccountIndexDTO> bankAccountIndexDTOs =
+                BankAccountTransformer.toBankAccountIndexDTO(bankAccounts);
+
+        Assertions.assertEquals(bankAccounts.size(), bankAccountIndexDTOs.size());
+
+        bankAccounts.stream().forEach(bankAccount -> {
+            Optional<BankAccountIndexDTO> optional = bankAccountIndexDTOs.stream()
+                    .filter(bankAccountIndexDTO ->
+                            bankAccountIndexDTO.getBankAccountId().equals(bankAccount.getId()))
+                    .findFirst();
+            Assertions.assertTrue(optional.isPresent());
+
+            final BankAccountIndexDTO bankAccountIndexDTO = optional.get();
+
+            Assertions.assertAll(
+                    () -> Assertions.assertEquals(
+                            bankAccount.getBankAccountName(), bankAccountIndexDTO.getBankAccountName()),
+                    () -> Assertions.assertEquals(bankAccount.getId(), bankAccountIndexDTO.getBankAccountId()));
+        });
     }
 }
