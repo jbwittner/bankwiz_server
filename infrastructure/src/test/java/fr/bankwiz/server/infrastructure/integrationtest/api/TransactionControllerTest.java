2aeec9300eedb008507f1b2781fb9d5ec25de977
Jean-Baptiste WITTNER <jb.wittner@outlook.com>
2024-02-12 23:14:54 +0100

    yo recipes

--- infrastructure/src/test/java/fr/bankwiz/server/infrastructure/integrationtest/api/TransactionControllerTest.java
+++ infrastructure/src/test/java/fr/bankwiz/server/infrastructure/integrationtest/api/TransactionControllerTest.java
@@ -14,6 +14,7 @@ import fr.bankwiz.openapi.model.BankAccountTransactionsDTO;
 import fr.bankwiz.openapi.model.CreateTransactionRequest;
 import fr.bankwiz.openapi.model.TransactionDTO;
 import fr.bankwiz.openapi.model.TransactionIndexDTO;
+import fr.bankwiz.openapi.model.UpdateTransactionRequest;
 import fr.bankwiz.server.domain.model.data.BankAccount;
 import fr.bankwiz.server.domain.model.data.Group;
 import fr.bankwiz.server.domain.model.data.GroupRight.GroupRightEnum;
@@ -119,5 +120,44 @@ class TransactionControllerTest extends InfrastructureIntegrationTestBase {
                     TransactionTransformer.toTransactionIndexDTO(transactionFinded);
             Assertions.assertEquals(transactionIndexDTO, transactionIndexDTOFinded);
         });
+    }
+
+    @Test
+    void updateTransaction() {
+        final User user = this.factory.getUser();
+        final Jwt jwt = this.mockAuthentification(user);
+
+        final Group group = this.factory.getGroup();
+        this.factory.getGroupRight(group, user, GroupRightEnum.WRITE);
+        final BankAccount bankAccount = this.factory.getBankAccount(group);
+
+        final Transaction transaction = this.factory.getTransaction(bankAccount);
+
+        final UpdateTransactionRequest updateTransactionRequest = new UpdateTransactionRequest();
+        updateTransactionRequest.setComment(this.faker.rickAndMorty().character());
+        updateTransactionRequest.setDecimalAmount(this.faker.random().nextInt(Integer.MAX_VALUE));
+
+        final TransactionDTO response = given().auth()
+                .oauth2(jwt.getTokenValue())
+                .header("Content-type", "application/json")
+                .body(updateTransactionRequest)
+                .put("/transaction/" + transaction.getId())
+                .then()
+                .assertThat()
+                .statusCode(200)
+                .extract()
+                .as(TransactionDTO.class);
+
+        Assertions.assertAll(
+            () -> Assertions.assertEquals(transaction.getId(), response.getTransactionId()),
+            () -> Assertions.assertNotEquals(transaction.getComment(), response.getComment()),
+            () -> Assertions.assertNotEquals(transaction.getDecimalAmount(), response.getDecimalAmount()),
+            () -> Assertions.assertEquals(updateTransactionRequest.getComment(), response.getComment()),
+            () -> Assertions.assertEquals(updateTransactionRequest.getDecimalAmount(), response.getDecimalAmount())
+        );
+
+        Assertions.fail();
+        //Ajouter vérification enregistrement dans la bdd
+
     }
 }
