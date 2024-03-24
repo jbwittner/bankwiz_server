a31d6e5ca1e9fff5ce2987510d3bd7fcc6422285
Jean-Baptiste WITTNER <jb.wittner@outlook.com>
2024-03-24 15:09:27 +0100

    wip

--- domain/src/test/java/fr/bankwiz/server/domain/service/HelperDomain.java
+++ domain/src/test/java/fr/bankwiz/server/domain/service/HelperDomain.java
@@ -1,3 +1,11 @@
 package fr.bankwiz.server.domain.service;
 
-public class HelperDomain {}
+import fr.bankwiz.server.domain.model.User;
+
+import java.util.UUID;
+
+public class HelperDomain {
+    public HelperDomain() {
+        final User user = new User(UUID.randomUUID(), "", "");
+    }
+}
