b59d5d6c8187d6912d5002d89d45eecec2964acd
Jean-Baptiste WITTNER <jb.wittner@outlook.com>
2024-03-27 23:03:01 +0100

    All your codebase are belong to us.

--- infrastructure/src/main/java/fr/bankwiz/server/infrastructure/spi/database/entity/UserEntity.java
+++ infrastructure/src/main/java/fr/bankwiz/server/infrastructure/spi/database/entity/UserEntity.java
@@ -8,12 +8,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
-@Builder
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
