6e45a34c5079f678eab5c4d5041a1f012b885eaa
Jean-Baptiste WITTNER <jb.wittner@outlook.com>
2024-03-27 23:58:02 +0100

    happy monday _ bleh _

---
+++ infrastructure/src/test/java/fr/bankwiz/server/infrastructure/integrationtest/api/UserControllerTest.java
@@ -0,0 +1,63 @@
+package fr.bankwiz.server.infrastructure.integrationtest.api;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.oauth2.jwt.Jwt;
+
+import fr.bankwiz.openapi.model.UserDTO;
+import fr.bankwiz.server.domain.model.model.UserAuthenticationDomain;
+import fr.bankwiz.server.infrastructure.integrationtest.InfrastructureIntegrationTestBase;
+import fr.bankwiz.server.infrastructure.spi.AuthenticationSpiImpl;
+import fr.bankwiz.server.infrastructure.spi.database.entity.UserEntity;
+import fr.bankwiz.server.infrastructure.spi.database.repository.UserEntityRepository;
+
+import static io.restassured.RestAssured.given;
+
+public class UserControllerTest extends InfrastructureIntegrationTestBase {
+
+    @Autowired
+    private UserEntityRepository userEntityRepository;
+
+    @MockBean
+    private AuthenticationSpiImpl authenticationSpi;
+
+    @Test
+    void checkregistration() throws Exception {
+        final Jwt jwt = Jwt.withTokenValue("token")
+                .header("alg", "none")
+                .claim("scope", "message:read")
+                .build();
+
+        Mockito.when(this.jwtDecoder.decode(jwt.getTokenValue())).thenReturn(jwt);
+
+        final String email = faker.internet().emailAddress();
+        final String sub = this.factory.getAuthId();
+
+        final UserAuthenticationDomain userAuthenticationDomain = new UserAuthenticationDomain(sub, email);
+
+        Mockito.when(this.authenticationSpi.getUserAuthentication()).thenReturn(userAuthenticationDomain);
+
+        final UserDTO response = given().auth()
+                .oauth2(jwt.getTokenValue())
+                .get("/user/checkregistration")
+                .then()
+                .assertThat()
+                .statusCode(200)
+                .extract()
+                .as(UserDTO.class);
+
+        Assertions.assertEquals(email, response.getEmail());
+
+        final Optional<UserEntity> optional = this.userEntityRepository.findById(response.getId());
+
+        Assertions.assertAll(
+                () -> Assertions.assertTrue(optional.isPresent()),
+                () -> Assertions.assertEquals(email, optional.get().getEmail()),
+                () -> Assertions.assertEquals(sub, optional.get().getAuthId()));
+    }
+}
